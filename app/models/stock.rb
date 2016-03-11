09fd2f20a543dfbd249499fa0e3a2d61edb89f6f
Camron Moore <camronbmoore@gmail.com>
2016-03-11 13:51:44 +0000

    Added stock rules and limit

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,7 @@ class Stock < ActiveRecord::Base
   has_many :users, through: :user_stocks
   
   def self.find_by_ticker(ticker_symbol)
-    where(ticker: ticker_symbol).first
+    where(ticker:ticker_symbol).first
   end
   
   def self.new_from_lookup(ticker_symbol)
